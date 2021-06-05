0522dc87a78716696a3f3af2d745c3a01fd00854
ganesh tiwari <gat786@outlook.com>
2021-06-05 16:52:12 +0530

    created auth module

--- api/controllers/auth.ts
+++ api/controllers/auth.ts
@@ -1,1 +1,13 @@
- 
+import express from "express";
+
+const router = express.Router();
+
+router.get("/login", (req, res) => {
+  res.send("Requested recieved at login");
+});
+
+router.get("/register", (req, res) => {
+  res.send("Request received at Sign up");
+});
+
+export { router };
--- api/index.ts
+++ api/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import * as auth from "./controllers/auth";
 
 const app = express();
 
@@ -7,6 +8,8 @@ const PORT = process.env.PORT || 8000;
 app.get("/", (req, res) => {
   res.send("Express + Typescript Server Running Baby");
 });
+
+app.use("/auth", auth.router);
 
 app.listen(PORT, () => {
   console.log(`🔥Server Started at Port ${PORT}`);
